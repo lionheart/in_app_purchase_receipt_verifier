930e4808b9b90200448d95947d9769b79fed605d
Dan Loewenherz <dan@lionheartsw.com>
2017-10-27 09:33:00 -0500

    update playground

--- IAPVerifierKeyGenerator.playground/Sources/Helpers.swift
+++ IAPVerifierKeyGenerator.playground/Sources/Helpers.swift
@@ -1,40 +1,7 @@
 import Foundation
 
-public extension Data {
-    init(key: Key) throws {
-        var error: Unmanaged<CFError>?
-        guard let data = SecKeyCopyExternalRepresentation(key.key, &error) as Data? else {
-            throw error!.takeRetainedValue() as Error
-        }
-
-        self = data
-    }
-}
-
-public struct Key {
-    var key: SecKey
-
-    public var publicKey: Key? {
-        guard let _key = SecKeyCopyPublicKey(key) else {
-            return nil
-        }
-
-        return Key(key: _key)
-    }
-
-    public enum KeyClass {
-        case `public`
-        case `private`
-
-        var rawValue: CFString {
-            switch self {
-            case .public: return kSecAttrKeyClassPublic
-            case .private: return kSecAttrKeyClassPrivate
-            }
-        }
-    }
-
-    public init() throws {
+public extension SecKey {
+    static func generateBase64Encoded2048BitRSAKey() throws -> (private: String, public: String) {
         let type = kSecAttrKeyTypeRSA
         let attributes: [String: Any] =
             [kSecAttrKeyType as String: type,
@@ -42,37 +9,13 @@ public struct Key {
         ]
 
         var error: Unmanaged<CFError>?
-        guard let key = SecKeyCreateRandomKey(attributes as CFDictionary, &error) else {
-            throw error!.takeRetainedValue() as Error
+        guard let key = SecKeyCreateRandomKey(attributes as CFDictionary, &error),
+            let data = SecKeyCopyExternalRepresentation(key, &error) as Data?,
+            let publicKey = SecKeyCopyPublicKey(key),
+            let publicKeyData = SecKeyCopyExternalRepresentation(publicKey, &error) as Data? else {
+                throw error!.takeRetainedValue() as Error
         }
 
-        self.key = key
-    }
-
-    public init(key: SecKey) {
-        self.key = key
-    }
-
-    public init(base64EncodedString string: String, keyClass: KeyClass) throws {
-        let keyData = Data(base64Encoded: string)!
-
-        let options: [String: Any] = [kSecAttrKeyType as String: kSecAttrKeyTypeRSA,
-                                      kSecAttrKeyClass as String: keyClass.rawValue,
-                                      kSecAttrKeySizeInBits as String: 2048]
-        var error: Unmanaged<CFError>?
-        guard let key = SecKeyCreateWithData(keyData as CFData, options as CFDictionary, &error) else {
-            throw error!.takeRetainedValue() as Error
-        }
-
-        self.key = key
-    }
-
-    public func validateSignature(data: Data, signature: Data, algorithm: SecKeyAlgorithm = .rsaSignatureMessagePKCS1v15SHA256) -> Bool {
-        var error: Unmanaged<CFError>?
-        guard SecKeyVerifySignature(key, algorithm, data as CFData, signature as CFData, &error) else {
-            return false
-        }
-
-        return true
+        return (private: data.base64EncodedString(), public: publicKeyData.base64EncodedString())
     }
 }
